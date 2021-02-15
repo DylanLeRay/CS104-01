700afd3e2c1b605c9f7d1d9da89e4e2fa3be7a07
DylanLeRay <79094479+DylanLeRay@users.noreply.github.com>
2021-02-15 09:54:27 -0500

    This is version 1 of my hello world program

--- helloWorld.py
+++ helloWorld.py
@@ -1,7 +1,2 @@
-Python 3.8.7 (v3.8.7:6503f05dd5, Dec 21 2020, 12:45:15) 
-[Clang 6.0 (clang-600.0.57)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
->>> # This is Version 1 of my hello world program
->>> print("Hello my name is Dylan")
-Hello my name is Dylan
->>> 
+# This is Version 1 of my hello world program
+print("Hello my name is Dylan")
