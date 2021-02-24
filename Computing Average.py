b20522c600f07077f3eb7a24f40bd20d75b22c21
DylanLeRay <79094479+DylanLeRay@users.noreply.github.com>
2021-02-24 00:21:26 -0500

    Add files via upload

---
+++ Computing Average.py
@@ -0,0 +1,17 @@
+howManyEntered = 0
+total = 0
+average = 0
+#this link inputs how many test scores user wants to enter
+howMany = input("How many test scores would you like to average?")
+print(howMany)
+howMany = 5
+testScore = 80
+howManyEntered = 0 + 1
+while howManyEntered <= howMany:
+    print("howManyEntered= ", howManyEntered)
+    howManyEntered = 0 + 1
+    total = 0 + testScore
+average = total / howMany
+
+
+
